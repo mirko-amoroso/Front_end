cafafd7e41b3223f541dc7e914207ceab9312eee
mirko-amoroso <mirkoamoroso599@gmail.com>
2024-06-04 17:04:09 +0200

    esercizio_2g

---
+++ 3_Mese/3°Settimana/2_Giorno/project_2g/src/app/interfacce/forms.ts
@@ -0,0 +1,10 @@
+export interface Forms {
+  nome: string,
+  cognome:string,
+  password: string,
+  conferma_password: string,
+  genere: string,
+  immagine_profilo: string,
+  biografia: string,
+  username: string
+}
